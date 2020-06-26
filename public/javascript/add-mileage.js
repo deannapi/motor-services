c09f45f6d3372e149af3016f8d39ede86927a1d5
deannapi <deannapi.mart@gmail.com>
2020-06-25 22:06:41 -0500

    adding login and logbook

--- public/javascript/add-mileage.js
+++ public/javascript/add-mileage.js
@@ -4,7 +4,22 @@ async function addMileageFormHandler(event) {
     const date = document.querySelector('input[name="date"]').value;
     const mileage = document.querySelector('input[name="mileage"]').value;
 
+    const response = await fetch(`/api/maintenance`, {
+        method: 'POST',
+        body: JSON.stringify({
+            date,
+            mileage
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 
+    if (response.ok) {
+        document.location.replace('/logbook');
+    } else {
+        alert(response.statusText);
+    }
 }
 
-document.querySelector('.logbook').addEventListener('submit', addMileageFormHandler);
+document.querySelector('#add-mileage').addEventListener('submit', addMileageFormHandler);
---
+++ public/javascript/cost.js
@@ -0,0 +1,3 @@
+
+
+document.querySelector('#add-cost').addEventListener('submit', addMileageFormHandler);
