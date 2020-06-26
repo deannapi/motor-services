5bfff8a5f1a5d8231497af54421f07597662b264
deannapi <deannapi.mart@gmail.com>
2020-06-26 13:02:31 -0500

    homepage login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,14 @@ const { User, Maintenance } = require('../models');
 router.get('/', (req, res) => {
     console.log(req.session);
     res.json(req.session);
-})
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render("login");
+});
 
 module.exports = router;
