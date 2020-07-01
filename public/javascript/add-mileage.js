9f524e9780026f1218c83db8cd012d40798ad40a
Paul Bradley <bradleypauld@gmail.com>
2020-06-30 20:17:56 -0500

    fix maintenance table building

--- public/javascript/add-mileage.js
+++ public/javascript/add-mileage.js
@@ -1,24 +1,32 @@
-async function addMileageFormHandler(event) {
-  event.preventDefault();
-
-  const date = document.querySelector('input[name="date_miles"]').value;
-  const mileage = document.querySelector('input[name="mileage"]').value;
-
+async function post(mileageData) {
+  debugger;
   const response = await fetch(`/api/maintenance`, {
     method: "POST",
-    body: JSON.stringify({
-      date,
-      mileage,
-    }),
+    body: mileageData,
     headers: {
       "Content-Type": "application/json",
     },
   });
 
   if (response.ok) {
-    document.location.replace("/mileage");
+    renderTable();
   } else {
-    alert(response.statusText);
+    console.log(response.statusText);
+  }
+}
+
+async function getAll() {
+  const response = await fetch('/api/maintenance', {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (response.ok) {  
+    return await response.json();
+  } else {
+    console.log(response.statusText);
   }
 }
 
@@ -38,86 +46,95 @@ var data = JSON.parse(localStorage.getItem("Mileage Logbook")) || {};
 // insert selected date and mileage into table
 var mileSubmit = document.getElementById("add-mileage");
 
-mileSubmit.addEventListener("click", function () {
+async function renderTable() {
+  const data = await getAll();
+
+  for (const i of data) {
+    //   Add date and mileage
+    const row = document.createElement("tr");
+    const td1 = document.createElement("td");
+    td1.innerText = i.date;
+    row.appendChild(td1);
+
+    const td2 = document.createElement("td");
+    td2.innerText = i.mileage;
+    row.appendChild(td2);
+
+    document.getElementById("mile_book").appendChild(row);
+    //   NEED TO ADD LOCAL STORAGE HERE - data deletes on refresh
+
+    // calculations for repair columns
+    var synOil = 3000;
+    var brakes = 50000;
+    var tires = 45000;
+    var filter = 300000;
+    var convOil = 7500;
+
+    //   Conventional Oil
+    const td3 = document.createElement("td");
+    if (i.mileage <= convOil) {
+      answer1 = convOil - i.mileage;
+      td3.innerText = answer1 + " more miles to go";
+
+    } else {
+      // color code RED if over 3000
+      td3.innerText = "Change Oil!";
+    }
+    row.appendChild(td3);
+    //   Synthetic Oil
+    const td4 = document.createElement("td");
+    if (i.mileage <= synOil) {
+      answer2 = synOil - i.mileage;
+      td4.innerText = answer2 + " more miles to go";
+
+    } else {
+      td4.innerText = "Change Oil!";
+    }
+    row.appendChild(td4);
+    // Brake Pads
+    const td5 = document.createElement("td");
+    if (i.mileage <= brakes) {
+      answer3 = brakes - i.mileage;
+      td5.innerText = answer3 + " more miles to go";
+
+    } else {
+      td5.innerText = "Change Brake Pads!";
+    }
+    row.appendChild(td5);
+
+    // Tires
+    const td6 = document.createElement("td");
+    if (i.mileage <= tires) {
+      answer4 = tires - i.mileage;
+      td6.innerText = answer4 + " more miles to go";
+
+    } else {
+      td6.innerText = "Change Tires!";
+    } row.appendChild(td6);
+
+    // Air Filter
+    const td7 = document.createElement("td");
+    if (i.mileage <= filter) {
+      answer5 = filter - i.mileage;
+      td7.innerText = answer5 + " more miles to go";
+      ;
+    } else {
+      td7.innerText = "Change Filter!";
+    } row.appendChild(td7)
+  }
+  //   on submit, clear input fields
+  document.getElementById("miles_form").reset();
+}
+
+mileSubmit.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get form data
   const date = document.getElementById("date_miles").value;
   const mileage = document.getElementById("miles").value;
-  console.log(date, mileage);
 
-  //   Add date and mileage
-  const row = document.createElement("tr");
-  const td1 = document.createElement("td");
-  td1.innerText = date;
-  row.appendChild(td1);
-
-  const td2 = document.createElement("td");
-  td2.innerText = mileage;
-  row.appendChild(td2);
-
-  document.getElementById("mile_book").appendChild(row);
-  //   NEED TO ADD LOCAL STORAGE HERE - data deletes on refresh
-
-  // calculations for repair columns
-  var synOil = 3000;
-  var brakes = 50000;
-  var tires = 45000;
-  var filter = 300000;
-  var convOil = 7500;
-
-  //   Conventional Oil
-  const td3 = document.createElement("td");
-  if (mileage < convOil) {
-    answer1 = convOil - mileage;
-    td3.innerText = answer1 + " more miles to go";
-    row.appendChild(td3);
-  } else if (mileage > convOil) {
-    // color code RED if over 3000
-    td3.innerText = "Change Oil!";
-  }
-
-  //   Synthetic Oil
-  const td4 = document.createElement("td");
-  if (mileage < synOil) {
-    answer2 = synOil - mileage;
-    td4.innerText = answer2 + " more miles to go";
-    row.appendChild(td4);
-  } else if (mileage > synOil) {
-    td4.innerText = "Change Oil!";
-  }
-
-  // Brake Pads
-  const td5 = document.createElement("td");
-  if (mileage < brakes) {
-    answer3 = brakes - mileage;
-    td5.innerText = answer3 + " more miles to go";
-    row.appendChild(td5);
-  } else if (mileage > brakes) {
-    td5.innerText = "Change Brake Pads!";
-  }
-
-  // Tires
-  const td6 = document.createElement("td");
-  if (mileage < tires) {
-    answer4 = tires - mileage;
-    td6.innerText = answer4 + " more miles to go";
-    row.appendChild(td6);
-  } else if (mileage > tires) {
-    td6.innerText = "Change Tires!";
-  }
-
-  // Air Filter
-  const td7 = document.createElement("td");
-  if (mileage < filter) {
-    answer5 = filter - mileage;
-    td7.innerText = answer5 + " more miles to go";
-    row.appendChild(td7);
-  } else if (mileage > filter) {
-    td7.innerText = "Change Filter!";
-  }
-
-    //   on submit, clear input fields
-    document.getElementById("miles_form").reset();
-
-    // save data to local storage
-    data = [date, mileage];
-    localStorage.setItem("Mileage Logbook", JSON.stringify(data));
+  // post to api with the form data
+  post({ date, mileage });
 });
+
+document.addEventListener("DOMContentLoaded", renderTable);
