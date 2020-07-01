35cf218eb7496309f755fddac6ce815d1e78e9ba
Conrad Trost <ctrost@tamu.edu>
2020-07-01 15:45:40 -0500

    Fixed minor route issue that produced headers error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,16 +5,20 @@ const { User, Maintenance } = require('../models');
 router.get('/welcome', (req, res) => {
     if (req.session.loggedIn) {
         res.render('welcome', {loggedIn: true});
+        return;
     }
+    res.render('homepage');
     return;
 });
 
 router.get('/', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/welcome');
+        return;
     }
 
     res.render('homepage');
+    return;
 });
 
 module.exports = router;
