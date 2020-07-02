89aca590e51488de40651a477243a137cebde0eb
Conrad Trost <ctrost@tamu.edu>
2020-07-01 20:42:37 -0500

    Added cost route to index api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const maintenanceRoute = require('./maintenance-routes');
 const userRoute = require('./user-routes');
+const costRoute = require('./cost-routes')
 
 router.use('/users', userRoute);
 router.use('/maintenance', maintenanceRoute);
+router.use('/cost', costRoute)
 
 module.exports = router;
